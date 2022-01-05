9b3e991d42ea371696c4da25a87b245c885d5c8f
Youssef <youssefboulmalf@gmail.com>
2022-01-05 20:04:17 +0100

    made responsive edits

--- src/ProjectData/projectData.js
+++ src/ProjectData/projectData.js
@@ -1,21 +1,21 @@
 export const projects = [
-        {
-                name: "Djammin",
-                description: `Djammin is a comission based social platform where dj's can create their own dj listing and bookers can book dj's directly.\n
+    {
+        name: "Virtual Portfolio",
+        description: `My virtual portfolio is made to show all projects I am and was working on.`,
+        language: [
+            "HTML", "CSS", "Javascript", "React", "NextJs"
+        ],
+        link: "",
+        imageName: "portfolio.jpeg"
+    },
+    {
+        name: "Djammin",
+        description: `Djammin is a comission based social platform where dj's can create their own dj listing and bookers can book dj's directly.\n
                 With over 70 diffrent listings you can choose a dj specifically to your unique needs.`,
-                language: [
-                    "HTML", "CSS", "Javascript", "React", "NodeJs"
-                ],
-                link: "https://djamm.in/",
-                imageName: "djammin.jpg"
-        },
-        {
-            name: "Virtual Portfolio",
-            description: `My virtual portfolio is made to show all projects I am and was working on.`,
-            language: [
-                "HTML", "CSS", "Javascript", "React", "NextJs"
-            ],
-            link: "",
-            imageName: "portfolio.jpeg"
+        language: [
+            "HTML", "CSS", "Javascript", "React", "NodeJs"
+        ],
+        link: "https://djamm.in/",
+        imageName: "djammin.jpg"
     }
-    ]
+]
