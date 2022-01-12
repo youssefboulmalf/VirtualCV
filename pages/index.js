3c9d6a69cc4cc81fef9376875bb8e77be5143137
Youssef <youssefboulmalf@gmail.com>
2022-01-12 16:04:57 +0100

    New project data

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
     <div className={css.container}>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
--- src/ProjectData/projectData.js
+++ src/ProjectData/projectData.js
@@ -1,14 +1,5 @@
 export const projects = [
     {
-        name: "Virtual Portfolio",
-        description: `My virtual portfolio is made to show all projects I am and was working on.`,
-        language: [
-            "HTML", "CSS", "Javascript", "React", "NextJs"
-        ],
-        link: "",
-        imageName: "portfolio.jpeg"
-    },
-    {
         name: "Djammin",
         description: `Djammin is a comission based social platform where dj's can create their own dj listing and bookers can book dj's directly.\n
                 With over 70 diffrent listings you can choose a dj specifically to your unique needs.`,
